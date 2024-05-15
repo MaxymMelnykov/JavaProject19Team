f96c7da49e16cb80840437b740b3be3ddf8f0747
VladSTp <136234442+VladSTp@users.noreply.github.com>
2024-05-15 20:21:55 +0300

    Update ReservationListener.java

--- src/main/java/com/javaproject19team/ReservationPackage/ReservationListener.java
+++ src/main/java/com/javaproject19team/ReservationPackage/ReservationListener.java
@@ -1,5 +1,10 @@
+/*
+ReservationListener:
+Інтерфейс для резервації
+*/
 package com.javaproject19team.ReservationPackage;
 
 public interface ReservationListener {
+    // Метод, який буде викликаний, коли резервація збережена
     void onReservationSaved(Reservation reservation);
 }
