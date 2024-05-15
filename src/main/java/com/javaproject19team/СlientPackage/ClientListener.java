581ecdd777a9ba576fe40da475b1de1e6fee7d19
VladSTp <136234442+VladSTp@users.noreply.github.com>
2024-05-15 20:34:28 +0300

    Update ClientListener.java

--- src/main/java/com/javaproject19team/СlientPackage/ClientListener.java
+++ src/main/java/com/javaproject19team/СlientPackage/ClientListener.java
@@ -1,7 +1,10 @@
+/*
+ReservationListener:
+Інтерфейс для клієнтів
+*/
 package com.javaproject19team.СlientPackage;
 
-
 public interface ClientListener {
+    // Метод, який буде викликаний, коли клієнт збережений
     void onClientSaved(Client client);
-
 }
