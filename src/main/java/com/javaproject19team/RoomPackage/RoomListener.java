e888befb4b97bab747458294515ebeb7e09162e4
VladSTp <136234442+VladSTp@users.noreply.github.com>
2024-06-02 17:13:21 +0300

    Update RoomListener.java

--- src/main/java/com/javaproject19team/RoomPackage/RoomListener.java
+++ src/main/java/com/javaproject19team/RoomPackage/RoomListener.java
@@ -1,6 +1,18 @@
+/*
+ReservationListener:
+Інтерфейс для номерів кімнат
+*/
 package com.javaproject19team.RoomPackage;
 
-
+/**
+ * ReservationListener:
+ * Інтерфейс для номерів кімнат
+ */
 public interface RoomListener {
+    /**
+     * Метод, який буде викликаний, коли кімната збережена
+     *
+     * @param room Збережена кімната
+     */
     void onRoomSaved(Room room);
 }
