defd68819157fe328fa57eafb3bcf5c8bd8722ca
VladSTp <136234442+VladSTp@users.noreply.github.com>
2024-06-02 17:12:52 +0300

    Update RoomList.java

--- src/main/java/com/javaproject19team/RoomPackage/RoomList.java
+++ src/main/java/com/javaproject19team/RoomPackage/RoomList.java
@@ -1,3 +1,7 @@
+/*
+RoomList:
+Клас, який відображає список номерів та надає можливість додавання нових номерів.
+*/
 package com.javaproject19team.RoomPackage;
 
 import com.javaproject19team.HotelReservationApp;
@@ -13,27 +17,50 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * RoomList:
+ * Клас, який відображає список номерів та надає можливість додавання нових номерів.
+ */
 public class RoomList extends Application {
     private RoomListener roomListener;
     private static Stage hotelReservationStage;
 
+    /**
+     * Метод для встановлення слухача подій для збереження номера кімнати
+     *
+     * @param roomListener Об'єкт слухача подій
+     */
     public void setRoomListener(RoomListener roomListener) {
         this.roomListener = roomListener;
     }
 
+    /**
+     * Метод для встановлення посилання на головне вікно додатку
+     *
+     * @param stage Головне вікно додатку
+     */
     public static void setHotelReservationStage(Stage stage) {
         hotelReservationStage = stage;
     }
 
+    /**
+     * Метод, що викликається при старті додатку.
+     *
+     * @param primaryStage головне вікно додатку
+     */
     @Override
     public void start(Stage primaryStage) {
+        // Ховаємо головне вікно додатку
         hotelReservationStage.hide();
+        // Заголовок вікна
         primaryStage.getIcons().add(new Image("file:src/main/resources/icon.png"));
         primaryStage.setTitle("Список номерів");
 
+        // Список кімнат
         ObservableList<Room> rooms = FXCollections.observableArrayList(HotelReservationApp.getRooms());
         TableView<Room> tableView = new TableView<>(rooms);
 
+        // Колонки таблиці
         TableColumn<Room, String> numberColumn = new TableColumn<>("Номер");
         numberColumn.setCellValueFactory(data -> data.getValue().numberProperty());
 
@@ -55,7 +82,7 @@ public class RoomList extends Application {
 
         tableView.getColumns().addAll(numberColumn, typeColumn, priceColumn, detailsColumn, statusColumn);
 
-
+        // Кнопка для додавання нового номера
         Button addButton = new Button("Додати");
         addButton.setOnAction(e -> showRoomAdd());
         addButton.setMinWidth(175);
@@ -63,6 +90,7 @@ public class RoomList extends Application {
         addButton.setFocusTraversable(false);
         addButton.setId("menu-button");
 
+        // Кнопка для оновлення списку номерів
         Button refreshButton = new Button("Оновити");
         refreshButton.setOnAction(e -> {
             rooms.clear();
@@ -73,6 +101,7 @@ public class RoomList extends Application {
         refreshButton.setFocusTraversable(false);
         refreshButton.setId("menu-button");
 
+        // Кнопка для повернення до головного меню
         Button onMainMenuButton = new Button("До головного меню");
         onMainMenuButton.setOnAction(e -> {
             primaryStage.hide();
@@ -83,9 +112,11 @@ public class RoomList extends Application {
         onMainMenuButton.setFocusTraversable(false);
         onMainMenuButton.setId("menu-button");
 
+        // HBox контейнер для кнопок
         HBox buttonsBox = new HBox(addButton, refreshButton, onMainMenuButton);
         buttonsBox.setSpacing(0);
 
+        // VBox контейнер для фільтрів
         VBox filterBox = new VBox();
         Label filterText = new Label("Фільтрація за номером, або типом, або по детальній інформації кімнати");
 
@@ -104,8 +135,8 @@ public class RoomList extends Application {
         filterBox.setAlignment(Pos.CENTER);
         filterBox.getChildren().addAll(filterText, filterField);
 
+        // Вертикальний контейнер для фільтрації за ціною
         VBox filterPriceVBox = new VBox();
-
         Label filterPriceLabel = new Label("Фільтрація за ціною");
 
         TextField minPriceField = new TextField();
@@ -118,7 +149,7 @@ public class RoomList extends Application {
         filterButton.setMinHeight(31);
         filterButton.setMaxHeight(31);
 
-
+        // Обробник події для кнопки фільтрації
         filterButton.setOnAction(event -> {
             String filter = filterField.getText().toLowerCase();
             double minPrice = minPriceField.getText().isEmpty() ? 0 : Double.parseDouble(minPriceField.getText());
@@ -131,16 +162,18 @@ public class RoomList extends Application {
             ));
         });
 
-
+        // HBox контейнер для полів та кнопки фільтрації за ціною
         HBox priceFilterBox = new HBox(minPriceField, maxPriceField, filterButton);
         priceFilterBox.setSpacing(10);
         priceFilterBox.setAlignment(Pos.CENTER);
         filterPriceVBox.setAlignment(Pos.CENTER);
         filterPriceVBox.getChildren().addAll(filterPriceLabel, priceFilterBox);
 
+        // VBox контейнер, що містить всі інші контроли
         VBox root = new VBox(buttonsBox, filterBox, filterPriceVBox, tableView);
         root.setSpacing(10);
 
+        // Scene, яка відображається у вікні
         Scene scene = new Scene(root, 520, 570);
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         primaryStage.setResizable(false);
@@ -148,12 +181,21 @@ public class RoomList extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Метод, який викликається при закритті вікна
+     *
+     * @throws Exception Виняток при закритті вікна
+     */
     @Override
     public void stop() throws Exception {
         super.stop();
+        // При закритті вікна повертаємо головне вікно додатку
         hotelReservationStage.show();
     }
 
+    /**
+     * Метод для відображення вікна додавання нового номера
+     */
     private void showRoomAdd() {
         Stage showRooms = new Stage();
         RoomEditor roomEditor = new RoomEditor();
@@ -164,7 +206,7 @@ public class RoomList extends Application {
             e.printStackTrace();
         }
     }
-
+    // Точка входу у програму
     public static void main(String[] args) {
         launch(args);
     }
