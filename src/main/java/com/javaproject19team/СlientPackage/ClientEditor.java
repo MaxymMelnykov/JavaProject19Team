7e0162a88eb7495dbfa685ca45a65aa332995f0f
VladSTp <136234442+VladSTp@users.noreply.github.com>
2024-06-02 17:14:20 +0300

    Update ClientEditor.java

--- src/main/java/com/javaproject19team/СlientPackage/ClientEditor.java
+++ src/main/java/com/javaproject19team/СlientPackage/ClientEditor.java
@@ -1,3 +1,7 @@
+/*
+ClientEditor:
+Цей клас відповідає за відображення вікна додавання нового клієнта та збереження введеної інформації.
+*/
 package com.javaproject19team.СlientPackage;
 
 import com.javaproject19team.DatabasePackage.DatabaseHandler;
@@ -13,16 +17,25 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 
+/**
+ * ClientEditor:
+ * Цей клас відповідає за відображення вікна додавання нового клієнта та збереження введеної інформації.
+ */
 public class ClientEditor extends Application {
     private ClientListener clientListener;
 
-
+    /**
+     * Метод, що викликається при старті додатку
+     *
+     * @param primaryStage Об'єкт вікна додатку
+     */
     @Override
     public void start(Stage primaryStage) {
+        // Налаштування вікна додавання нового клієнта
         primaryStage.getIcons().add(new Image("file:src/main/resources/icon.png"));
         primaryStage.setTitle("Додавання нового клієнта");
 
-        // Labels
+        // Інтерфейс введення даних про клієнта
         Label nameLabel = new Label("Ім'я:");
         TextField nameInput = new TextField();
         nameInput.setPromptText("Введіть ім'я");
@@ -55,11 +68,12 @@ public class ClientEditor extends Application {
         phoneHBox.setAlignment(Pos.CENTER);
         phoneHBox.setSpacing(10);
 
+        // Кнопка "Вийти"
         Button cancelButton = new Button("Вийти");
         cancelButton.setOnAction(e -> primaryStage.close());
         cancelButton.setMinWidth(100);
         cancelButton.setId("cancel-button");
-
+        //Кнопка "Зберегти"
         Button saveButton = new Button("Зберегти");
         saveButton.setOnAction(e -> saveClient(
                 new SimpleStringProperty(nameInput.getText()),
@@ -72,10 +86,12 @@ public class ClientEditor extends Application {
         HBox buttonHBox = new HBox(cancelButton, saveButton);
         buttonHBox.setSpacing(95);
 
+        // VBox для елементів інтерфейсу
         VBox root = new VBox(nameHBox, surnameHBox, emailHBox, phoneHBox, buttonHBox);
         root.setSpacing(10);
         root.setPadding(new Insets(10));
 
+        // Налаштування Scene та відображення вікна
         Scene scene = new Scene(root, 315, 210);
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         primaryStage.setResizable(false);
@@ -83,22 +99,38 @@ public class ClientEditor extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Метод для збереження даних про клієнта в базу даних
+     *
+     * @param clientName    Ім'я клієнта
+     * @param clientSurname Прізвище клієнта
+     * @param emailInput    Електронна пошта клієнта
+     * @param phoneNumber   Телефон клієнта
+     */
     private void saveClient(StringProperty clientName, StringProperty clientSurname, StringProperty emailInput, StringProperty phoneNumber) {
+        // Отримання даних про клієнта
         String clientNameDB = clientName.get();
         String clientSurnameDB = clientSurname.get();
         String emailDB = emailInput.get();
         String phoneDB = phoneNumber.get();
 
+        // Збереження даних про клієнта в базу даних
         DatabaseHandler.saveClientDB(clientNameDB, clientSurnameDB, emailDB, phoneDB);
         Client newClient = new Client(clientName, clientSurname, emailInput, phoneNumber);
         clientListener.onClientSaved(newClient);
 
     }
 
+    /**
+     * Метод для встановлення ClientListener
+     *
+     * @param listener Об'єкт слухача подій
+     */
     public void setClientListener(ClientListener listener) {
         this.clientListener = listener;
     }
 
+    // Точка входу у програму
     public static void main(String[] args) {
         launch(args);
     }
